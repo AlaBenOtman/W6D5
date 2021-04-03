65af762cb2e8d9845dcee9fc368fd7757dff9367
voldemort <alabenothman@gmail.com>
2021-04-03 15:34:26 -0700

    created index html files and made some edits

--- nightycats/db/seeds.rb
+++ nightycats/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cat1= Cat.create(birth_date: '02-08-1982', color: 'black', name: 'Alex', sex: 'M', description: 'naughty cat')
+cat2= Cat.create(birth_date: '02-08-1582', color: 'black', name: 'Jin', sex: 'F', description: 'good cat')
