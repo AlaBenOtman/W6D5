a9c0ef983cc8f3cbd178a97cccd10c29667aeb6a
voldemort <alabenothman@gmail.com>
2021-04-03 14:12:13 -0700

    migrated cats table and add more validation touches

--- nightycats/app/models/cat.rb
+++ nightycats/app/models/cat.rb
@@ -5,9 +5,8 @@ class Cat < ApplicationRecord
     AVILABLE_COLORS = %w(black)
     validates :size, inclusion: AVILABLE_COLORS
     validates :sex, inclusion: %w(M F)
+    validates :birth_date, :color, :name, :sex, :description,  presence: true, null: false 
   
-
-
 
     def age 
         time_ago_in_words(birth_date)
